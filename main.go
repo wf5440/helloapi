baf7542c87d7087afbd450ae2702dfc6ca9e06ee
wf5440 <wf3046529861@gmail.com>
2025-09-28 23:15:57 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "你好，发！这是你的第一个后端接口。")
+}
+
+func main() {
+	// Render 会自动提供 PORT 环境变量
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // 本地调试用
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Println("Server listening on port " + port)
+	http.ListenAndServe(":"+port, nil)
+}
